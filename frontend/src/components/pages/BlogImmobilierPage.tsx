045f7ff5fd78243924b9533b141ff9cb074a7b25
Oli Mcfly <66916000+olimcfly@users.noreply.github.com>
2025-06-13 11:31:20 +0200

    Fix main.tsx path in index.html

--- frontend/src/components/pages/BlogImmobilierPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const BlogImmobilierPage = () => (
-  <div className="min-h-screen flex items-center justify-center">
-    <h1 className="text-3xl font-bold">Blog Immobilier Premium</h1>
-  </div>
-);
-
-export default BlogImmobilierPage;
